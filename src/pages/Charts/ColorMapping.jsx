efd064e6874c43271676afebc6e23330096bbc1d
lekkss <leksguy97@gmail.com>
2022-07-27 12:15:06 +0100

    done

---
+++ src/pages/Charts/ColorMapping.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../components";
+const ColorMapping = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Chart" title="Color Mapping" />
+    </div>
+  );
+};
+
+export default ColorMapping;
---
+++ src/pages/Charts/Line.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../components";
+const Line = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Chart" title="Line" />
+    </div>
+  );
+};
+
+export default Line;
